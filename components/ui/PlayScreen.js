3a95ce55d6c3fd0a05818adfe942bfee4649f958
Divine Orji <dpkreativ@gmail.com>
2022-12-26 05:38:46 +0100

    Create play screen

---
+++ components/ui/PlayScreen.js
@@ -0,0 +1,24 @@
+import useSound from 'use-sound';
+import { PlayIcon } from '../assets/Icons';
+
+export default function PlayScreen({ onClick }) {
+  // function handleClick() {
+  //   play;
+  //   onClick;
+  // }
+  const [play] = useSound(`/audio/audio.mp3`);
+  return (
+    <div className="flex flex-col space-y-48">
+      <div>Put on your headphones</div>
+      <div className="flex flex-col space-y-6 items-center justify-center">
+        <div>Tap this</div>
+        <div className="relative w-max" onClick={play}>
+          <PlayIcon />
+          <div className="absolute top-0 left-0 rounded-full animate-[ping_3s_cubic-bezier(0,0,0.2,1)_infinite] opacity-10">
+            <PlayIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
