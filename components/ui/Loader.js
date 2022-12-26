d8d3087bfb37615258ab69bc4e7c922be13e7799
Divine Orji <dpkreativ@gmail.com>
2022-12-26 05:38:20 +0100

    Create Loader component

---
+++ components/ui/Loader.js
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="h-screen flex flex-col space-y-48 items-center justify-center">
+      <img src="/images/loading.gif" alt="loading" />;
+    </div>
+  );
+}
